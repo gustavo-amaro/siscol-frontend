f1c7763bb3be5a70bc17ea05c21a640250d8fdd9
gustavo <gustavo.amaro97@gmail.com>
2020-05-23 11:57:24 -0300

    feat: add loading component

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -3,14 +3,10 @@ import React from "react";
 import { Container } from "./styles";
 import { HashLoader } from "react-spinners";
 
-
 export default function Login() {
   return (
     <Container>
-        <HashLoader
-            size={50}
-            color={"#682ab5"}
-          />
+      <HashLoader size={50} color={"#fff"} />
     </Container>
   );
 }
--- src/components/Loading/styles.js
+++ src/components/Loading/styles.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  position: absolute;
   display: flex;
   width: 100%;
   height: 100%;
   justify-content: center;
   align-items: center;
+  background: #682ab5;
 `;
